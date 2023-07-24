6e64ad15b20cc879359f029f0a0a12fa7175fe0f
Hondajr <mitsuo.hondajr@gmail.com>
2023-07-24 15:50:50 -0300

    Nodemon Inst
    Instalação e config básica do nodemon.

---
+++ Node/index.js
@@ -0,0 +1,18 @@
+//Importando um objeto atraves do 'require'
+const {Person} = require("./person");
+
+//Inicializamos um objeto pessoa, passando o parametro requerido
+const person = new Person("Honda");
+
+//Testamos o metodo
+console.log(person.sayMyName());
+
+//'package-lock.json', lista as versões dos modulos que estão sendo usados no projeto.
+
+/*  O package Nodemon, funciona como o liveserver, executando automaticamente ao salvar.
+      Lembrando que em caso o nodemon seja instalado localmente, como nesse caso, devemos configurar um comando no package.json:
+        `${nome do comando}: "nodemon index.js"`
+      e iniciamos o nodemon com o comando npm run ${nome do comando}
+      Caso o nodemon esteja instalado globalmente, podemos simplesmente executar:
+        `nodemon index.js`
+*/
